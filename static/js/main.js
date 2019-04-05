3fd1dd849b4b1aef16ec60f81b5fcfb00dfceb34
Meitar M <meitarm@gmail.com>
2019-04-05 19:01:05 -0400

    Insert course information in application fee link.

--- static/js/main.js
+++ static/js/main.js
@@ -9,9 +9,11 @@
         const p = new URLSearchParams(window.location.search);
         const a = document.querySelector('#payment-options [href^="https://www.paypal.com/cgi-bin/webscr"]');
         if (a) {
+            a.parentElement.style.display = 'list-item';
             let qs = new URLSearchParams(a.search);
             qs.set('custom', p.get('custom') || '');
-            qs.set('item_name', p.get('course') || '');
+            qs.set('item_name', p.get('course') + ' ' + qs.get('item_name') || '');
+            qs.set('item_number', p.get('course') + qs.get('item_number') || '');
             const url = a.protocol + "//" + a.host + a.pathname + "?" + qs.toString();
             a.setAttribute('href', url);
         }
