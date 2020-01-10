8050d9acbc4b8d3ffa37afc4a4c6814a615b8318
Meitar M <meitarm@gmail.com>
2020-01-10 16:07:40 -0500

    Refactor all JavaScript to use ES2015 modules, strict mode.

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,8 @@
 /**
  * Main script.
  */
-(function () {
-    /**
-     * Updates the payment link with relevant additional client info.
-     */
-    function updateLink () {
-        const p = new URLSearchParams(window.location.search);
-        const a = document.querySelector('#payment-options [href^="https://www.paypal.com/cgi-bin/webscr"]');
-        if (a) {
-            a.parentElement.style.display = 'list-item';
-            let qs = new URLSearchParams(a.search);
-            qs.set('custom', p.get('custom') || '');
-            qs.set('item_name', p.get('course') + ' ' + qs.get('item_name') || '');
-            qs.set('item_number', p.get('course') + qs.get('item_number') || '');
-            const url = a.protocol + "//" + a.host + a.pathname + "?" + qs.toString();
-            a.setAttribute('href', url);
-        }
-    }
 
-    /**
-     * Updates the page text to reflect the current payee.
-     */
-    function updateText () {
-        const p = new URLSearchParams(window.location.search);
-        const el = document.getElementById('course-id')
-        if (el && p.get('course')) {
-            el.textContent = p.get('course');
-        }
-    }
-
-    document.addEventListener('DOMContentLoaded', updateLink);
-    document.addEventListener('DOMContentLoaded', updateText);
-})();
+// This file intentionally left blank.
+//
+// JavaScript is currently invoked on a per-page basis from
+// within IIFEs via dynamically imported ES2015 modules.
---
+++ static/js/modules/application-invoice.mjs
@@ -0,0 +1,43 @@
+/**
+ * Application invoice module.
+ */
+
+/**
+ * Updates the payment link with relevant additional client info.
+ */
+function updateLink () {
+    const p = new URLSearchParams(window.location.search);
+    const a = document.querySelector('#payment-options [href^="https://www.paypal.com/cgi-bin/webscr"]');
+    if (a) {
+        a.parentElement.style.display = 'list-item';
+        let qs = new URLSearchParams(a.search);
+        qs.set('custom', p.get('custom') || '');
+        qs.set('item_name', p.get('course') + ' ' + qs.get('item_name') || '');
+        qs.set('item_number', p.get('course') + qs.get('item_number') || '');
+        const url = a.protocol + "//" + a.host + a.pathname + "?" + qs.toString();
+        a.setAttribute('href', url);
+    }
+}
+
+/**
+ * Updates the page text to reflect the current payee.
+ */
+function updateText () {
+    const p = new URLSearchParams(window.location.search);
+    const el = document.getElementById('course-id')
+    if (el && p.get('course')) {
+        el.textContent = p.get('course');
+    }
+}
+
+/**
+ * Zhu Li! Do the thing!
+ */
+function main () {
+    updateLink();
+    updateText();
+}
+
+export default function () {
+    main();
+}
