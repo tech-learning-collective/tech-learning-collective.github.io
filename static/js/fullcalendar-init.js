9c5e62dc72d12cbae5bc7095638507dcfe88a98e
fabacab <fabacab@riseup.net>
2020-06-11 14:49:21 -0400

    Add pricing and seat capacity info to event calendar.

--- static/js/fullcalendar-init.js
+++ static/js/fullcalendar-init.js
@@ -29,6 +29,58 @@ layout: none
                 : 'listWeek'; // Default view.
         }
 
+        /**
+         * Custom event item renderer.
+         *
+         * @see https://fullcalendar.io/docs/eventRender
+         *
+         * @param object info Event information.
+         */
+        function renderEvent (info) {
+            if (info.view.type.match(/^list/)) {
+                var style = info.el.style;
+                style.height             = '5em';
+                style.backgroundImage    = 'url("' + info.event.extendedProps.image + '")';
+                style.backgroundRepeat   = 'no-repeat';
+                // TODO: Move this into CSS proper.
+                if ('portrait' === calendarViewLayout()) {
+                    style.backgroundPosition = 'bottom left';
+                    style.backgroundSize     = '140px';
+                } else {
+                    style.backgroundPosition = 'right';
+                    style.backgroundSize     = 'contain';
+                }
+
+                var p = document.createElement('p');
+                if (new Date(info.event.end) > new Date()) {
+                    if (info.event.extendedProps.schemaDotOrg.offers) {
+                        var offers = info.event.extendedProps.schemaDotOrg.offers
+                        var prices = offers.reduce(function (a, c) {
+                            return {
+                                'min': (c.price < a.price) ? c.price: a.price,
+                                'max': (c.price > a.price) ? c.price: a.price,
+                                'price': a.price,
+                                'priceCurrency': a.priceCurrency
+                            };
+                        });
+                        p.innerHTML = '<a href="' + info.event.url + '#tickets-'
+                            + info.event.url.split('/').pop()
+                            + '">Buy tickets ($' + parseFloat(prices.min) + '&ndash;$'
+                            + parseFloat(prices.max) + ' ' + prices.priceCurrency + ')</a>'
+                    } else {
+                        p.innerHTML = 'Free or donation-based';
+                    }
+
+                    if (info.event.extendedProps.schemaDotOrg.maximumAttendeeCapacity) {
+                        p.innerHTML += ' &mdash; Seat limit: ' + info.event.extendedProps.schemaDotOrg.maximumAttendeeCapacity;
+                    }
+                } else {
+                    p.textContent = 'This event has passed.'
+                }
+                info.el.querySelector('.fc-list-item-title').appendChild(p);
+            }
+        }
+
         var el  = document.getElementById('fullcalendar');
         var cal = new FullCalendar.Calendar(el, {
             'plugins': [
@@ -43,32 +95,7 @@ layout: none
             'aspectRatio': ('portrait' === calendarViewLayout()) ? 0.5 : 1.35,
             'defaultView': getFullCalendarView(),
             'events': fullcalendar_events || '{% link events/all-fullcalendar-io.json %}',
-            'eventRender': function (info) {
-                if (info.view.type.match(/^list/)) {
-                    var style = info.el.style;
-                    style.height             = '5em';
-                    style.backgroundImage    = 'url("' + info.event.extendedProps.image + '")';
-                    style.backgroundRepeat   = 'no-repeat';
-                    // TODO: Move this into CSS proper.
-                    if ('portrait' === calendarViewLayout()) {
-                        style.backgroundPosition = 'bottom left';
-                        style.backgroundSize     = '140px';
-                    } else {
-                        style.backgroundPosition = 'right';
-                        style.backgroundSize     = 'contain';
-                    }
-
-                    var p = document.createElement('p');
-                    if (new Date(info.event.end) > new Date()) {
-                        p.innerHTML = (info.event.extendedProps.schemaDotOrg.offers)
-                            ? '<a href="' + info.event.url + '#tickets-' + info.event.url.split('/').pop() + '">Buy tickets</a>'
-                            : 'Free or donation-based';
-                    } else {
-                        p.textContent = 'This event has passed.'
-                    }
-                    info.el.querySelector('.fc-list-item-title').appendChild(p);
-                }
-            },
+            'eventRender': renderEvent,
             'windowResize': function (view) {
                 if ('portrait' === calendarViewLayout()) {
                     // Adjust for newer, small window size.
